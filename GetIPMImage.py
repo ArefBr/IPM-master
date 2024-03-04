708f32bd084f1816f0e38f6c6ef8d53fc45e2aa2
Aref Borhani <108584817+ArefBr@users.noreply.github.com>
2024-03-04 15:49:07 +0330

    Update GetIPMImage.py

--- GetIPMImage.py
+++ GetIPMImage.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 from GetInfo import GetInfo
 from GetVanishingPoint import GetVanishingPoint
 from TransformImage2Ground import TransformImage2Ground
@@ -5,7 +6,7 @@ from TransformGround2Image import TransformGround2Image
 import cv2
 import numpy as np
 
-
+# Define a class to store camera information
 class Info(object):
     def __init__(self, dct):
         self.dct = dct
@@ -13,39 +14,55 @@ class Info(object):
     def __getattr__(self, name):
         return self.dct[name]
 
+# Specify the picture number and file details
 pic = 16
-I = cv2.imread('images/road3.jpg')
+file_name = 'processed_60_1705496977213150464'
+file_type = '.jpg'
+folder = '/home/aref/Desktop/IPM/Images/0_30_45_60/'
 
+# Read the image
+I = cv2.imread(folder + file_name + file_type)
+
+# Extract image dimensions
 R = I[:, :, :]
-height = int(I.shape[0]) # row y
-width = int(I.shape[1]) # col x
+height = int(I.shape[0]) # number of rows (y)
+width = int(I.shape[1]) # number of columns (x)
 
+# Define camera information
 cameraInfo = Info({
-    "focalLengthX": int(width / 100) * 100, # 1200.6831,         # focal length x
-    "focalLengthY": int(height / 100) * 100, # 1200.6831,         # focal length y
-    "opticalCenterX": int(width / 2), # 638.1608,        # optical center x
-    "opticalCenterY": int(height / 2), # 738.8648,       # optical center y
-    "cameraHeight": 1500, # 1879.8,  # camera height in `mm`
-    "pitch": 2.5,           # rotation degree around x
-    "yaw": 0,              # rotation degree around y
-    "roll": 0              # rotation degree around z
+    "focalLengthX": 1567.9328940076439,
+    "focalLengthY": 1563.5710737727954,
+    "opticalCenterX": 915.9938327378984,
+    "opticalCenterY": 451.13296389625447,
+    "cameraHeight": 100,  # in mm
+    "pitch": 60,           # rotation around x-axis
+    "yaw": 0.0,            # rotation around y-axis
+    "roll": 0              # rotation around z-axis
 })
+
+# Define IPM (Inverse Perspective Mapping) information
 ipmInfo = Info({
     "inputWidth": width,
     "inputHeight": height,
-    "left": 50,
-    "right": width-50,
-    "top": 2600,
-    "bottom": height
+    "left": 0,
+    "right": width-1,
+    "top": 0,
+    "bottom": height-1
 })
-# IPM
+
+# Find vanishing point
 vpp = GetVanishingPoint(cameraInfo)
 vp_x = vpp[0][0]
 vp_y = vpp[1][0]
+
+# Update top value in IPM information
 ipmInfo.top = float(max(int(vp_y), ipmInfo.top))
+
+# Define UV limits for IPM
 uvLimitsp = np.array([[vp_x, ipmInfo.right, ipmInfo.left, vp_x],
             [ipmInfo.top, ipmInfo.top, ipmInfo.top, ipmInfo.bottom]], np.float32)
 
+# Transform UV limits to ground coordinates
 xyLimits = TransformImage2Ground(uvLimitsp, cameraInfo)
 row1 = xyLimits[0, :]
 row2 = xyLimits[1, :]
@@ -54,6 +71,8 @@ xfMax = max(row1)
 yfMin = min(row2)
 yfMax = max(row2)
 xyRatio = (xfMax - xfMin)/(yfMax - yfMin)
+
+# Initialize output image
 outImage = np.zeros((640,960,4), np.float32)
 outImage[:,:,3] = 255
 outRow = int(outImage.shape[0])
@@ -63,6 +82,7 @@ stepCol = (xfMax - xfMin)/outCol
 xyGrid = np.zeros((2, outRow*outCol), np.float32)
 y = yfMax-0.5*stepRow
 
+# Generate XY grid for ground coordinates
 for i in range(0, outRow):
     x = xfMin+0.5*stepCol
     for j in range(0, outCol):
@@ -71,16 +91,18 @@ for i in range(0, outRow):
         x = x + stepCol
     y = y - stepRow
 
-# TransformGround2Image
+# Transform ground coordinates to image coordinates
 uvGrid = TransformGround2Image(xyGrid, cameraInfo)
-# mean value of the image
+
+# Calculate mean value of the image
 means = np.mean(R)/255
 RR = R.astype(float)/255
+
+# Interpolate image values for the output image
 for i in range(0, outRow):
     for j in range(0, outCol):
         ui = uvGrid[0, i*outCol+j]
         vi = uvGrid[1, i*outCol+j]
-        #print(ui, vi)
         if ui < ipmInfo.left or ui > ipmInfo.right or vi < ipmInfo.top or vi > ipmInfo.bottom:
             outImage[i, j] = 0.0
         else:
@@ -94,10 +116,13 @@ for i in range(0, outRow):
             outImage[i, j, 1] = float(RR[y1, x1, 1])*(1-x)*(1-y)+float(RR[y1, x2, 1])*x*(1-y)+float(RR[y2, x1, 1])*(1-x)*y+float(RR[y2, x2, 1])*x*y
             outImage[i, j, 2] = float(RR[y1, x1, 2])*(1-x)*(1-y)+float(RR[y1, x2, 2])*x*(1-y)+float(RR[y2, x1, 2])*(1-x)*y+float(RR[y2, x2, 2])*x*y
 
+# Set the last row of the output image to zeros
 outImage[-1,:] = 0.0 
-# show the result
 
+# Convert the output image to integer values (0-255) and save it
 outImage = outImage * 255
-print("finished");
-# save image
-cv2.imwrite('processed/pp.png',outImage)
+print(f"{file_name} Finished.")
+print(f"Height: {cameraInfo.cameraHeight},\nPitch: {cameraInfo.pitch},\nYaw: {cameraInfo.yaw},\nRoll: {cameraInfo.roll}.\n")
+
+# Save the output image
+cv2.imwrite(f'processed/p{cameraInfo.pitch}_h{cameraInfo.cameraHeight}_{file_name}.png',outImage)
